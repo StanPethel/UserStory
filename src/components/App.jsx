eb15562a302cd2dad2366487dc238e07bce8746d
Stanley Pethel <stanleypethel@yahoo.com>
2021-11-12 16:22:48 -0500

    updatedFiles

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return(
+          <h1> My React App from User Story</h1>  
+        )
+    }
+}
+
+export default App;
